efe2fde000c75bf5ce70897b9f35adff399dcc39
강혜미 <eham1003@naver.com>
2023-10-15 21:06:56 +0900

    feat: post 반환 개수 설정

--- src/main/java/com/example/leica_refactoring/post/PostRepository.java
+++ src/main/java/com/example/leica_refactoring/post/PostRepository.java
@@ -6,14 +6,15 @@ import com.example.leica_refactoring.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Transactional
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findAllByOrderByCreatedAtDesc();
 
+    List<Post> findTop5ByOrderByCreatedAtDesc();
     List<Post> findByMemberMemberId(String username);
     List<Post> findByChildCategory(Category category);
 
