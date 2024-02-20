ea4cdf54870f2ece8763d9b566889333c68910e0
seonghun <sdzx0719@naver.com>
2024-02-20 22:24:50 +0900

    jwt(fix) : at, rt 만료시간 수정

--- src/main/java/com/example/leica_refactoring/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/leica_refactoring/jwt/JwtTokenProvider.java
@@ -40,9 +40,9 @@ public class JwtTokenProvider {
     @Value("${jwt.secret}")
     private String secretKey;
 
-    private long accessTokenValidTime = 30 * 1000L;
+    private long accessTokenValidTime = 30 * 60 * 1000L;
     //    private long refreshTokenValidTime = 7 * 24 * 60 * 60 * 1000L; // 7d
-    private long refreshTokenValidTime = 60 * 1000L;
+    private long refreshTokenValidTime = 7 * 24 * 60 * 60 * 1000L;
 
     @PostConstruct
     public void init() {
